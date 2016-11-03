5877d4874c152fdeb87c0d5f2d41642d857017ab
flavius <flavius.popoiu@mediaimage.ro>
2016-11-03 17:14:08 +0200

    battle

--- chapter1/startjourney.py
+++ chapter1/startjourney.py
@@ -9,13 +9,35 @@ whereTo = 0
 
 continuity = True
 
+def letsFight():
+    heroHealth = random.randint(40,50)
+    orcHealth = random.randint(35,45)
+    heroDamage = random.randint(4,6)
+    orcDamage = random.randint(3,5)
+    continueFight = True
+    while continueFight is True:
+        heroHealth -= orcDamage
+        orcHealth -= heroDamage
+        print("The enemy does {} damage and the hero has {} health left".format(orcDamage, heroHealth))
+        print("The hero does {} damage and the enemy has {} health left".format(heroDamage, orcHealth))
+        if heroHealth <= 0:
+            print("You died")
+            continueFight = False
+        elif orcHealth <= 0:
+            print("You are victorious")
+            continueFight = False
+
+def revealOccupant(hutsie, whatHut):
+    if hutsie[whatHut] == 'enemy':
+        print("You're in a battle now, bitch")
+        letsFight()
+    elif hutsie[whatHut] == 'friend':
+        print("You win, there is a friend inside")
+    elif hutsie[whatHut] == 'unoccupied':
+        print("You can rest, the place is empty")
+
 while keep_playin == 'y':
     whereTo = int(input("Choose between five huts "))
     whereTo -= 1
-    if huts[whereTo] == 'enemy':
-        print("You lost, there is an enemy inside")
-    elif huts[whereTo] == 'friend':
-        print("You win, there is a friend inside")
-    elif huts[whereTo] == 'unoccupied':
-        print("You can rest, the place is empty")
+    revealOccupant(huts, whereTo)
     keep_playin = input("Do you want to continue? ")
